be4f44bb13c8d65628bbfdfa85c6c186e2fe5171
Justin Pridgen <52604018+bokuwaboingo@users.noreply.github.com>
2021-04-14 23:56:34 -0400

    Added types

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+declare class Licenses {
+	public static List: string[];
+	public static AFL: string;
+	public static APACHE: string;
+	public static ART: string;
+	public static BSL: string;
+	public static BSD2C: string;
+	public static BSD3C: string;
+	public static BSD3CC: string;
+	public static CC0: string;
+	public static CCBY: string;
+	public static CCBYSA: string;
+	public static WTFPL: string;
+	public static ECL: string;
+	public static EPL1: string;
+	public static EPL2: string;
+	public static EUPL: string;
+	public static AGPL: string;
+	public static GPL2: string;
+	public static GPL3: string;
+	public static LGPL21: string;
+	public static LGPL3: string;
+	public static ISC: string;
+	public static LPPL: string;
+	public static MSPL: string;
+	public static MIT: string;
+	public static MPL: string;
+	public static OSL: string;
+	public static POSTSQL: string;
+	public static OFL: string;
+	public static NCSA: string;
+	public static UNL: string;
+	public static ZLIB: string;
+}
+
+export = Licenses;
