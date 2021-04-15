7efdd2b8451dd6484c2af3a4746787f3665f1aa4
Justin Pridgen <52604018+bokuwaboingo@users.noreply.github.com>
2021-04-15 15:03:17 -0400

    REJECT JAVASCRIPT, BECOME TYPESCRIPT (2.0.0)

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const path = require("path");
-const Licenses = require("..");
+import fs = require("fs");
+import path = require("path");
+import arg = require("arg");
+import Licenses = require("..");
 
-const args = require("arg")({
+const args = arg({
 	"--output": String,
 	"--name": String,
 	"--year": Number,
@@ -46,28 +47,32 @@ const list = [
 	"zLib License",
 ]
 
-function log(...data) {
+function log(...data: string[]) {
 	console.log(data.join("\n"));
 }
 
-if (!process.argv[2]) log(
-	"Format: licenses (license) (option(s))",
-	"",
-	"Options:",
-	"--output/-n?: The output of the license",
-	"--name/-n: The name of the licensor",
-	"--year/-y?: The year the license was created",
-	"",
-	"List of Licenses:",
-	list.join("\n")
-);
-else if (Object.keys(Licenses).some(x => new RegExp(args._[0], "i").test(x))) {
-	if (!args["--name"]) return log("Please specify a name.");
-	try {
-		fs.writeFileSync(args["--output"] || path.resolve(process.cwd(), "LICENSE"), Licenses[args._[0]](args["--name"], args["--year"] || new Date().getFullYear()));
-		log(`License written to ${args["--output"] || path.resolve(process.cwd(), "LICENSE")}.`);
-	} catch (error) {
-		if (error.errno == -4068) log("This is the name of a directory! Please try again.");
-		else log("An error occurred when writing the license! Please try again.");
-	}
-} else if (!Object.keys(Licenses).some(x => new RegExp(args._[0], "i").test(x))) log("Please specify a valid license name.");
+function cli() {
+	if (!process.argv[2]) log(
+		"Format: licenses (license) (option(s))",
+		"",
+		"Options:",
+		"--output/-n?: The output of the license",
+		"--name/-n: The name of the licensor",
+		"--year/-y?: The year the license was created",
+		"",
+		"List of Licenses:",
+		list.join("\n")
+	);
+	else if (Object.keys(Licenses).some(x => new RegExp(args._[0], "i").test(x))) {
+		if (!args["--name"]) return log("Please specify a name.");
+		try {
+			fs.writeFileSync(args["--output"] || path.resolve(process.cwd(), "LICENSE"), Licenses[args._[0]](args["--name"], args["--year"] || new Date().getFullYear()));
+			log(`License written to ${args["--output"] || path.resolve(process.cwd(), "LICENSE")}.`);
+		} catch (error) {
+			if (error.errno == -4068) log("This is the name of a directory! Please try again.");
+			else log("An error occurred when writing the license! Please try again.");
+		}
+	} else if (!Object.keys(Licenses).some(x => new RegExp(args._[0], "i").test(x))) log("Please specify a valid license name.");
+}
+
+cli();
